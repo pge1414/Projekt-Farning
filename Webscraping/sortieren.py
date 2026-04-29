ad631fbb8affaae4c620c1fbd1cf3917906be38c
Paul Geiger <101663790+pge1414@users.noreply.github.com>
2026-04-29 16:54:56 +0200

    a

--- Webscraping/sortieren.py
+++ Webscraping/sortieren.py
@@ -2,6 +2,7 @@ import json
 import duden
 import time, tqdm
 from collections import deque
+import multiprocessing
 
 # TODO: - ausreizen wie viele wörter man durchlaufen kann, duden abbricht - gerade 86
 
@@ -13,19 +14,7 @@ with open('found_words.json', 'r') as f:
         for i in json.loads(line).values():
             wörter.append(i)
 
-
-flat_list = [item for sublist in wörter for item in tqdm.tqdm(sublist, desc="Flache Liste wird erstellt")]
-flat_list = [item.lower() for item in flat_list]
-anzahlen = {i: flat_list.count(i) for i in tqdm.tqdm(set(flat_list), desc="Anzahlen werden berechnet")}
-
-anzahlen = dict(sorted(anzahlen.items(), key=lambda item: item[1]))
-letzte_elemente = deque(anzahlen.items(), maxlen=90)
-anzahlen = dict(letzte_elemente)
-print(anzahlen)
-print(len(anzahlen))
-anzahlen2 = anzahlen.copy()
-print(anzahlen2)
-for word in tqdm.tqdm(anzahlen2.keys(), desc="Verarbeitung läuft"):
+def synonyme_ergänzen():
     if duden.get(word) is not None:
         synonyme = duden.get(word).synonyms
         if synonyme:
@@ -38,7 +27,32 @@ for word in tqdm.tqdm(anzahlen2.keys(), desc="Verarbeitung läuft"):
                 if synonym in anzahlen.keys():
                     anzahlen[word] += anzahlen[synonym]
                     del anzahlen[synonym]
-                    time.sleep(0.1)
+                    time.sleep(0.0)
+
+flat_list = [item for sublist in wörter for item in tqdm.tqdm(sublist, desc="Flache Liste wird erstellt")]
+flat_list = [item.lower() for item in flat_list]
+anzahlen = {i: flat_list.count(i) for i in tqdm.tqdm(set(flat_list), desc="Anzahlen werden berechnet")}
+
+anzahlen = dict(sorted(anzahlen.items(), key=lambda item: item[1]))
+letzte_elemente = deque(anzahlen.items(), maxlen=100)
+anzahlen = dict(letzte_elemente)
+print(anzahlen)
+print(len(anzahlen))
+anzahlen2 = anzahlen.copy()
+print(anzahlen2)
+for word in tqdm.tqdm(anzahlen2.keys(), desc="Verarbeitung läuft"):
+    synonyme_ergänzen()
+
 print(anzahlen)
 print(len(anzahlen))
 print(max(anzahlen, key=anzahlen.get))
+
+# def url_sortieren():
+#     with open('links.csv', 'r') as f:
+#         flat_list = [item for f in wörter for item in tqdm.tqdm(f, desc="Flache Liste wird erstellt")]
+#         for i in tqdm.tqdm(flat_list, desc="Verarbeitung läuft"):
+#             if i in flat_list:
+#                 flat_list.remove(i)
+#     with open('links.csv', 'w') as f:
+#         for item in tqdm.tqdm(flat_list, desc="Datei wird geschrieben"):
+#             f.write("%s\n" % item)
---
+++ Webscraping/webscrapingfinal.py
@@ -0,0 +1,158 @@
+#https://realpython.com/python-web-scraping-practical-introduction/
+#https://brightdata.de/blog/web-data-de/how-to-scrape-news-articles
+#https://brightdata.com/blog/how-tos/beautiful-soup-web-scraping
+#https://beautiful-soup-4.readthedocs.io/en/latest/#navigating-the-tree
+#https://www.geeksforgeeks.org/python/multiprocessing-python-set-1/
+#notion.site
+#["([['a']])"] - wie bekomme ich die unnötigen klammern weg, so dass danach nur eine liste mit einem a drin steht? - url.append(u.values()[0]) - so könnte
+
+#Problem: links.json muss stets geleert werden, damit die neuen Links hinzugefügt werden können, da sonst die alten Links mit den neuen vermischt werden. Lösung: links.json wird mit jedem neuen Link überschrieben, damit nur die neuen Links gespeichert werden.
+#str(urlf) in link.get("href", "") and 
+
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import ssl
+import certifi
+import csv
+import json
+import tqdm
+import requests
+import multiprocessing
+
+context = ssl.create_default_context(cafile=certifi.where())
+url = []
+wörter = []
+threads = []
+keinegewichtungen = []
+unwichtigelinks = []
+
+
+def linkchecker(urlf):
+    for i in unwichtigelinks:
+        if i in urlf:
+            return False
+    return True
+
+
+def url_aus_csv():
+    with open('links.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in spamreader:
+            for i in row:
+                url.append(i)
+    print(url)
+
+def get_keinegewichtungen():
+    with open('keinegewichtungen.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in spamreader:
+            for i in row:
+                keinegewichtungen.append(i) 
+
+def url_sortieren():
+    with open('links.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        flat_list = []
+        for row in spamreader:
+            for i in row:
+                flat_list.append(i)
+        saubere_liste = []
+        for item in flat_list:
+            if item not in saubere_liste:
+                saubere_liste.append(item)
+        print(f"Liste: {spamreader}")
+        print(f"Flache Liste: {flat_list}")
+        print(f"Saubere Liste: {saubere_liste}")
+    with open('links.csv', 'w') as f:
+        for item in tqdm.tqdm(saubere_liste, desc="Datei wird geschrieben"):
+            f.write("%s\n" % item)
+
+def get_unwichtigelinks():
+    with open('unwichtigelinks.csv', newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in spamreader:
+            for i in row:
+                unwichtigelinks.append(i)
+
+def scrape_link(urlf):
+    links = []
+    
+    
+    if requests.get(urlf).status_code == 200:
+        page = urlopen(urlf, context=context)
+        html = page.read().decode("utf-8")
+        soup = BeautifulSoup(html, "html.parser")
+        get_keinegewichtungen()
+        get_unwichtigelinks()
+
+        for link in soup.find_all("a"):
+            href = link.get("href", "")
+            if href not in url and linkchecker(href):
+                if not href.endswith(".pdf") and href.startswith(urlf):
+                    links.append(href)
+        with open('links.csv', 'a', newline='') as csvfile:
+            spamwriter = csv.writer(csvfile, delimiter=' ',quotechar='|', quoting=csv.QUOTE_MINIMAL)
+            spamwriter.writerow(links)
+
+scrape_link("https://www.handelsblatt.com")
+#
+
+def links_aufrufen(url):
+    wörter = []
+
+    if requests.get(url).status_code == 200:
+    
+        page = urlopen(url, context=context)
+        html = page.read().decode("utf-8")
+        soup = BeautifulSoup(html, "html.parser")
+        for i in soup.get_text().split():
+            if i not in wörter and len(i) > 2 and i not in keinegewichtungen:
+
+                #print("Found!" + " "  + j)
+                wörter.append(i)
+
+        with open("found_words.json", "a") as f:
+            f.write(json.dumps({url: wörter}, ensure_ascii=False) + "\n")
+
+if __name__ == "__main__":
+    url_aus_csv()
+    for i in tqdm.tqdm(range(3, len(url), 4), desc="Verarbeitung läuft"):
+
+        p1 = multiprocessing.Process(target=links_aufrufen, args=(url[i],))
+        p2 = multiprocessing.Process(target=links_aufrufen, args=(url[i-1],))
+        p3 = multiprocessing.Process(target=links_aufrufen, args=(url[i-2],))
+        p4 = multiprocessing.Process(target=links_aufrufen, args=(url[i-3],))
+        p5 = multiprocessing.Process(target=scrape_link, args=(url[i],))
+        p6 = multiprocessing.Process(target=scrape_link, args=(url[i-1],))
+        p7 = multiprocessing.Process(target=scrape_link, args=(url[i-2],))
+        p8 = multiprocessing.Process(target=scrape_link, args=(url[i-3],))
+        p1.start()
+        p2.start()
+        p3.start()
+        p4.start()
+        p5.start()
+        p6.start()
+        p7.start()
+        p8.start()
+
+        p1.join()
+        p2.join()
+        p3.join()
+        p4.join()
+        p5.join()
+        p6.join()
+        p7.join()
+        p8.join()
+    url_sortieren()
+
+# from tqdm import tqdm
+
+# liste = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
+# ergebnis = []
+
+# # Start bei Index 3 ("D"), Schrittweite 4
+# for i in tqdm(range(3, len(liste), 4), desc="Extrahiere"):
+#     ergebnis.append(liste[i])
+
+# print(ergebnis)
+# # Ausgabe: ['D', 'H'] -> für 16 statt 4
